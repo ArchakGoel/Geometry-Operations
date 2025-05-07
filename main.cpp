7ff572146b20a73af7b7a0a7b53b942a78d4e284
ArcherG007 <41598373+ArchakGoel@users.noreply.github.com>
2025-05-07 15:10:51 +0530

    vector class with main.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "vector3D.h"
+
+int main() {
+
+  entities::vector3D p1(2, 2, 1);
+
+  entities::vector3D p2(4, 2, 1);
+
+  auto dotVal = p1.dot(p2);
+
+  std::cout << dotVal;
+
+  // class test {
+  // private:
+  //   int member1;
+  //   double second;
+  //   std::string chaera;
+
+  // public:
+  //   int rando;
+  //   test() { int a = 2; }
+  // };
+
+  return 0;
+}
