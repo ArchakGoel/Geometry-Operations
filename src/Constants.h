a9556a13b5a72d30e3f744e1ae53dff86936e224
ArcherG007 <41598373+ArchakGoel@users.noreply.github.com>
2025-06-20 00:49:07 +0530

    added constants file

---
+++ src/Constants.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cmath>
+
+namespace precision{
+
+const double LINEAR = 1.0e-7;
+const double ANGULAR = 1.0e-12;
+
+
+}
+
+namespace constants{
+
+    //other constants with apt. name of namespace for that category.
+}
