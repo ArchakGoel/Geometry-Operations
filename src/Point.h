272c2a1edfe3e855079536ff5bad0210b190f46f
ArcherG007 <41598373+ArchakGoel@users.noreply.github.com>
2025-06-28 23:00:59 +0530

    lots of updates to Point
    1. added == and + operators, as yet to make a base class for vec and point. And point is not a vec.
    2. added getter methods
    3. removed inheritance from vector3D.

--- src/Point.h
+++ src/Point.h
@@ -1,15 +1,27 @@
 #pragma once
-
-#include "Vector3D.h"
-
 namespace entities {
 
-  class Point : public Vector3D {
+//!brief: dot and cross product don't apply to points. So no vector3D form.
+  class Point{
+    double x, y, z;
+
   public:
     explicit Point(const double x, const double y, const double z)
-        : Vector3D(x, y, z) {};
-    explicit Point() : Vector3D(0.0, 0.0, 0.0) {};
-    explicit Point(Vector3D vector):Vector3D(vector){};
+        :x(x), y(y), z(z) {};
+    explicit Point() : x(0.0), y(0.0), z(0.0) {};
+
+    //todo: use precision here.
+    bool operator==(const Point &rhs) const {
+    return ((x == rhs.x) && (y == rhs.y) && (z == rhs.z));
+  }
+    Point operator+(const Point& rhs) const {
+      return Point(x+rhs.x, y+rhs.y, z+rhs.z);
+    }
+
+    double getX()const {return x;}
+    double getY()const {return y;}
+    double getZ()const {return z;}
+    //todo: write copy constr'
   };
 
 }
