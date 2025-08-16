56dbf4d5960601185dbbcdd22a630a6af4a7e259
ArcherG007 <41598373+ArchakGoel@users.noreply.github.com>
2025-08-16 23:10:59 +0530

    main, comment out

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,20 @@
 
 int main() {
 
-  // a few initialisations of various Entities objects for smoke tests.
-  Entities::Vector3D p1{3, 2, 1};
+  // a few examples of initialisations of various Entities objects.
+  // Entities::Vector3D p1{3, 2, 1};
 
-  Entities::Vector3D p2(4, 2, 1);
+  // Entities::Vector3D p2(4, 2, 1);
 
-  Entities::Vector3D p3;
+  // Entities::Vector3D p3;
 
-  std::cout << p3 << std::endl;
+  // std::cout << p3 << std::endl;
 
-  Entities::Line l1(Entities::Point(p1.getX(), p1.getY(), p1.getZ()), Entities::Point(p2.getX(), p2.getY(), p2.getZ())); 
+  // Entities::Line l1(Entities::Point(p1.getX(), p1.getY(), p1.getZ()), Entities::Point(p2.getX(), p2.getY(), p2.getZ())); 
 
-  Entities::Plane plane1(Entities::Point(p1.getX(), p1.getY(), p1.getZ()), p1);
+  // Entities::Plane plane1(Entities::Point(p1.getX(), p1.getY(), p1.getZ()), p1);
 
-  auto dotVal = p1.dot(p2);
-
-  std::cout << "main is generating output. Dot is " << dotVal;
+  // auto dotVal = p1.dot(p2);
 
   return 0;
 }
